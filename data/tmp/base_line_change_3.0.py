571adf91404623a8dbb4e460c2d841bb6747a393
whs <1612255875@qq.com>
2020-07-05 18:35:37 +0800

    123

--- data/tmp/base_line_change_3.0.py
+++ data/tmp/base_line_change_3.0.py
@@ -45,9 +45,48 @@ def acc_combo(y, y_pred):
 
 def get_data(run_new=False, add=False):
     if add:
-        pass
+        df = pd.read_csv('tmp.csv', index_col=0)
 
-    if not os.path.exists('tmp.csv') or run_new:
+        # data['acc_x_diff'] = data['acc_x'].diff()
+        # data['acc_y_diff'] = data['acc_y'].diff()
+        # data['acc_z_diff'] = data['acc_z'].diff()
+
+        data['acc_xc'] = data['acc_xg'] - data['acc_x']
+        data['acc_yc'] = data['acc_yg'] - data['acc_y']
+        data['acc_zc'] = data['acc_zg'] - data['acc_z']
+        data['G'] = (data['acc_xc'] ** 2 + data['acc_yc'] ** 2 + data['acc_zc'] ** 2) ** 0.5
+
+        data['delta_xy'] = np.arcsin(data['acc_zc'] / data['G'])
+        data['delta_xz'] = np.arcsin(data['acc_yc'] / data['G'])
+        data['delta_yz'] = np.arcsin(data['acc_xc'] / data['G'])
+
+        data['acc_xc_diff'] = data['acc_xc'].diff()
+        data['acc_yc_diff'] = data['acc_yc'].diff()
+        data['acc_zc_diff'] = data['acc_zc'].diff()
+
+        data['acc'] = (data['acc_x'] ** 2 + data['acc_y'] ** 2 + data['acc_z'] ** 2) ** 0.5
+        data['accg'] = (data['acc_xg'] ** 2 + data['acc_yg'] ** 2 + data['acc_zg'] ** 2) ** 0.5
+        for f in tqdm([f for f in data.columns if f in [
+            'acc_x', 'acc_y', 'acc_z', 'acc',
+            'acc_xg', 'acc_yg', 'acc_zg', 'accg',
+            'acc_xc', 'acc_yc', 'acc_zc', 'G',
+            'delta_xy', 'delta_xz', 'delta_yz',
+            'acc_x_diff', 'acc_y_diff', 'acc_z_diff',
+            'acc_xc_diff', 'acc_yc_diff', 'acc_zc_diff',
+        ]]):
+            # for stat in ['min', 'max', 'mean', 'median', 'std', 'skew']:
+            #     # for stat in ['min', 'max', 'mean', 'median', 'std']:
+            #     df[f + '|' + stat] = data.groupby('fragment_id')[f].agg(stat).values
+
+            df[f + '|' + 'qtl_02'] = data.groupby('fragment_id')[f].quantile(0.02).values
+            df[f + '|' + 'qtl_98'] = data.groupby('fragment_id')[f].quantile(0.98).values
+            # df[f + '|' + 'power_rate'] = data.groupby('fragment_id')[f].apply(lambda x: (x.abs() > 0.1).sum() / len(x))
+            # df[f + '|' + '95_05'] = df[f + '|' + 'qtl_95'] - df[f + '|' + 'qtl_05']
+            # df[f + '|' + '80_20'] = df[f + '|' + 'qtl_80'] - df[f + '|' + 'qtl_20']
+        df.to_csv('tmp.csv')
+        return df
+
+    elif not os.path.exists('tmp.csv') or run_new:
         df = data.drop_duplicates(subset=['fragment_id']).reset_index(drop=True)[['fragment_id', 'behavior_id']]
 
         # data['acc_x_diff'] = data['acc_x'].diff()
@@ -119,6 +158,7 @@ def get_data(run_new=False, add=False):
 
 label = 'behavior_id'
 df = get_data()
+# df = get_data()
 df['move_range'] = (df['acc_xc|max_min'] + df['acc_yc|max_min'] + df['acc_zc|max_min'])
 train_df = df[df[label].notna()].reset_index(drop=True)
 test_df = df[df[label].isna()].reset_index(drop=True)
@@ -177,33 +217,40 @@ drop_feat = []
 #                  # 'peaks_num',
 #              ]
 #              ]
-# used_feat = [f for f in train_df.columns
-#              if f.split('|')[0] in [
-#                  'acc_x', 'acc_y', 'acc_z', 'acc',
-#                  'acc_xg', 'acc_yg', 'acc_zg', 'accg',
-#                  'acc_xc', 'acc_yc', 'acc_zc', 'G',
-#                  'delta_xy', 'delta_xz', 'delta_yz',
-#              ] and f.split('|')[1] in [
-#                  # 'min', 'max',
-#                  'mean',
-#                  'median',
-#                  'std',
-#                  # '95_05',
-#                  # '80_20',
-#                  # 'qtl_05',
-#                  # 'qtl_95',
-#                  # 'qtl_20',
-#                  # 'qtl_80',
-#                  # 'out_num',
-#                  # 'max_min',
-#                  # 'peaks_num',
-#              ]
-#              ] + ['acc_yg|qtl_05', 'acc_yg|qtl_95', 'acc_xg|qtl_05', 'acc_xg|qtl_95']
+used_feat = [f for f in train_df.columns
+             if f.split('|')[0] in [
+                 'acc_x', 'acc_y', 'acc_z', 'acc',
+                 'acc_xg', 'acc_yg', 'acc_zg', 'accg',
+                 'acc_xc', 'acc_yc', 'acc_zc', 'G',
+                 # 'delta_xy', 'delta_xz', 'delta_yz',
+             ] and f.split('|')[1] in [
+                 # 'min', 'max',
+                 'mean',
+                 'median',
+                 'std',
+                 # '95_05',
+                 # '80_20',
+                 # 'qtl_02',
+                 # 'qtl_98',
+                 # 'qtl_20',
+                 # 'qtl_80',
+                 # 'out_num',
+                 # 'max_min',
+                 # 'peaks_num',
+             ]] + ['acc_yg|qtl_05', 'acc_yg|qtl_95', 'acc_xg|qtl_05', 'acc_xg|qtl_95'] + [
+                f for f in train_df.columns
+                if f.split('|')[0] in ['acc_x', 'acc_y', 'acc_z', 'acc'] and f.split('|')[1] in [
+        'min', 'max'
+    ]
+            ]
+
 # used_feat = [f for f in train_df.columns if f not in (['fragment_id', label] + drop_feat)]
-used_feat = ['G|mean', 'acc_xg|qtl_05', 'acc_xg|qtl_95', 'acc_yg|qtl_95', 'acc_yg|mean', 'G|std',
-             'acc_z|mean', 'acc_y|std', 'acc_x|mean', 'acc|median', 'acc_x|std', 'acc|std', 'acc_zc|std', 'accg|median',
-             'G|median', 'acc_yg|qtl_05', 'acc_yc|std', 'acc_zg|mean', 'acc_y|mean', 'acc_yg|std', 'acc_xg|std',
-             'accg|mean', 'acc_xg|mean']
+# used_feat = ['G|mean', 'acc_xg|qtl_05', 'acc_xg|qtl_95', 'acc_yg|qtl_95', 'acc_yg|mean', 'G|std',
+#              'acc_z|mean', 'acc_y|std', 'acc_x|mean', 'acc|median', 'acc_x|std', 'acc|std', 'acc_zc|std', 'accg|median',
+#              'G|median', 'acc_yg|qtl_05', 'acc_yc|std', 'acc_zg|mean', 'acc_y|mean', 'acc_yg|std', 'acc_xg|std',
+#              'accg|mean', 'acc_xg|mean']
+# used_feat = ['G|std', 'acc_yg|mean', 'acc_yg|qtl_95', 'acc_xg|mean', 'acc_yg|qtl_05', 'acc_y|std', 'G|median',
+#              'acc_y|mean', 'acc_xg|qtl_05', 'acc_zg|mean', 'acc|median', 'acc_yc|std', 'acc_xg|qtl_95']
 print(len(used_feat))
 print(used_feat)
 
@@ -297,6 +344,27 @@ print(model_name)
 #       num_leaves)
 print(score)
 print('*****')
+score_df = pd.concat([train_y, pred_y], axis=1)
+# d = score_df[train_y != pred_y]
+
+res_list = []
+for id, part_df in score_df.groupby('behavior_id'):
+    tmp_sr = part_df[0].value_counts().sort_index()
+    tmp_sr.name = id
+    # print(id, tmp_sr)
+    res_list.append(tmp_sr)
+a = pd.concat(res_list, axis=1)
+
+#
+# b = a[a>=17]
+# tmp_list = []
+# for i in range(19):
+#     for j in range(i):
+#         print(a.loc[i, j] + a.loc[i, j])
+#         tmp_list.append([f'{i}_{j}', a.loc[i, j] + a.loc[i, j]])
+# cor_df = pd.DataFrame(tmp_list)
+# cor_df = cor_df.sort_values(1, ascending=False)
+
 # res_all_list.append(
 #     [feature_fraction, bagging_fraction, lambda_l1, lambda_l2, num_iterations,
 #      max_depth, num_leaves, score])
@@ -310,6 +378,8 @@ print('*****')
 # use_list = list(set(gain_list) | set(split_list))
 # print(use_list)
 # print(len(use_list))
+# print(len(set(used_feat) - set(use_list)))
+# print(set(used_feat) - set(use_list))
 # a = set(use_list) & set(used_feat)
 # print(len(set(use_list) - set(used_feat)))
 # print(len(set(used_feat) - set(use_list)))
