d97e0858bcf2bdda4c6f87088bf3173ae571c549
whs <1612255875@qq.com>
2020-07-07 21:34:30 +0800

    123

--- data/tmp/base_line_4.0.py
+++ data/tmp/base_line_4.0.py
@@ -10,9 +10,12 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import StratifiedKFold
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
-sub = pd.read_csv('submit.csv')
+train = pd.read_csv('sensor_train.csv')
+test = pd.read_csv('sensor_test.csv')
+
+# train = pd.read_csv('train.csv')
+# test = pd.read_csv('test.csv')
+sub = pd.read_csv('提交结果示例.csv')
 y = train.groupby('fragment_id')['behavior_id'].min()
 
 train['mod'] = (train.acc_x ** 2 + train.acc_y ** 2 + train.acc_z ** 2) ** .5
@@ -58,38 +61,40 @@ def Net():
 
 kfold = StratifiedKFold(5, shuffle=True)
 
-proba_t = np.zeros((7500, 19))
-for fold, (xx, yy) in enumerate(kfold.split(x, y)):
-    y_ = to_categorical(y, num_classes=19)
-    model = Net()
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(),
-                  metrics=['acc'])
-    plateau = ReduceLROnPlateau(monitor="val_acc",
-                                verbose=0,
-                                mode='max',
-                                factor=0.1,
-                                patience=6)
-    early_stopping = EarlyStopping(monitor='val_acc',
-                                   verbose=0,
-                                   mode='max',
-                                   patience=10)
-    checkpoint = ModelCheckpoint(f'fold{fold}.h5',
-                                 monitor='val_acc',
-                                 verbose=0,
-                                 mode='max',
-                                 save_best_only=True)
-    model.fit(x[xx], y_[xx],
-              epochs=100,
-              batch_size=512,
-              verbose=1,
-              shuffle=True,
-              validation_data=(x[yy], y_[yy]),
-              callbacks=[plateau, early_stopping, checkpoint])
-    model.load_weights(f'fold{fold}.h5')
-    proba_t += model.predict(t, verbose=0, batch_size=1024) / 5.
-
-
-sub.behavior_id = np.argmax(proba_t, axis=1)
-print(sub)
-sub.to_csv('submit_cnn.csv', index=False)
+# proba_t = np.zeros((7500, 19))
+# for fold, (xx, yy) in enumerate(kfold.split(x, y)):
+#     y_ = to_categorical(y, num_classes=19)
+#     model = Net()
+#     model.compile(loss='categorical_crossentropy',
+#                   optimizer=Adam(),
+#                   metrics=['acc'])
+#     plateau = ReduceLROnPlateau(monitor="val_acc",
+#                                 verbose=0,
+#                                 mode='max',
+#                                 factor=0.1,
+#                                 patience=6)
+#     early_stopping = EarlyStopping(monitor='val_acc',
+#                                    verbose=0,
+#                                    mode='max',
+#                                    patience=10)
+#     checkpoint = ModelCheckpoint(f'fold{fold}.h5',
+#                                  monitor='val_acc',
+#                                  verbose=0,
+#                                  mode='max',
+#                                  save_best_only=True)
+#     model.fit(x[xx], y_[xx],
+#               epochs=100,
+#               batch_size=512,
+#               verbose=1,
+#               shuffle=True,
+#               validation_data=(x[yy], y_[yy]),
+#               callbacks=[plateau, early_stopping, checkpoint])
+#     model.load_weights(f'fold{fold}.h5')
+#     proba_t += model.predict(t, verbose=0, batch_size=1024) / 5.
+#
+#
+# sub.behavior_id = np.argmax(proba_t, axis=1)
+# print(sub)
+# sub.to_csv('submit_cnn.csv', index=False)
+a = ''
+a.split()
