5f6353398e2a1a38a9111a9ee904db77f844a63c
whs <1612255875@qq.com>
2020-07-10 08:35:52 +0800

    123

--- data/tmp/base_line_4.2.py
+++ data/tmp/base_line_4.2.py
@@ -6,78 +6,85 @@ from torch.autograd import Variable
 from sklearn.model_selection import KFold, RepeatedKFold, StratifiedKFold
 import torch.optim as optim
 import numpy as np
-import pandas as pd
-import lightgbm as lgb
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
-from sklearn.preprocessing import LabelEncoder
-# import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy.stats import entropy
-import gc
-import os
 from tqdm import tqdm
-
+from scipy.signal import resample
 
 pd.set_option('display.max_columns', 600)
 pd.set_option('display.max_rows', 600)
-from IPython.core.interactiveshell import InteractiveShell
 
-InteractiveShell.ast_node_interactivity = "all"
+root_path = './'
+train = pd.read_csv(root_path + 'sensor_train.csv')
+test = pd.read_csv(root_path + 'sensor_test.csv')
+sub = pd.read_csv(root_path + '提交结果示例.csv')
+y = train.groupby('fragment_id')['behavior_id'].min()
 
-data_path = ''
-data_train = pd.read_csv(data_path + 'sensor_train.csv')
-data_test = pd.read_csv(data_path + 'sensor_test.csv')
-data_test['fragment_id'] += 10000
-label = 'behavior_id'
 
-data = pd.concat([data_train, data_test], sort=False)
+def fun(data):
+    data['acc_xc'] = data['acc_xg'] - data['acc_x']
+    data['acc_yc'] = data['acc_yg'] - data['acc_y']
+    data['acc_zc'] = data['acc_zg'] - data['acc_z']
+    data['G'] = (data['acc_xc'] ** 2 + data['acc_yc'] ** 2 + data['acc_zc'] ** 2) ** 0.5
+    data['mod'] = (data.acc_x ** 2 + data.acc_y ** 2 + train.acc_z ** 2) ** .5
+    data['modg'] = (data.acc_xg ** 2 + data.acc_yg ** 2 + train.acc_zg ** 2) ** .5
+    return data
 
-df = data.drop_duplicates(subset=['fragment_id']).reset_index(drop=True)[['fragment_id', 'behavior_id']]
 
-a = data.drop_duplicates('fragment_id')
-b = data.drop_duplicates(subset=['fragment_id'])
+train = fun(train)
+test = fun(test)
+use_feat = [f for f in train.columns if f not in ['fragment_id', 'time_point', 'behavior_id']]
 
-data['acc'] = (data['acc_x'] ** 2 + data['acc_y'] ** 2 + data['acc_z'] ** 2) ** 0.5
-data['accg'] = (data['acc_xg'] ** 2 + data['acc_yg'] ** 2 + data['acc_zg'] ** 2) ** 0.5
+# train = train[use_feat]
+# test = test[use_feat]
 
-for f in tqdm([f for f in data.columns if 'acc' in f]):
-    for stat in ['min', 'max', 'mean', 'median', 'std', 'skew']:
-        df[f + '_' + stat] = data.groupby('fragment_id')[f].agg(stat).values
+x = np.zeros((7292, 60, len(use_feat), 1))
+t = np.zeros((7500, 60, len(use_feat), 1))
+for i in tqdm(range(7292)):
+    tmp = train[train.fragment_id == i][:60]
+    x[i, :, :, 0] = resample(tmp[use_feat], 60, np.array(tmp.time_point))[0]
 
-train_df = df[df[label].isna() == False].reset_index(drop=True)
-test_df = df[df[label].isna() == True].reset_index(drop=True)
-train_df
-
-drop_feat = []
-used_feat = [f for f in train_df.columns if f not in (['fragment_id', label] + drop_feat)]
-
-train_x = train_df[used_feat]
-train_y = train_df[label]
-test_x = test_df[used_feat]
+for i in tqdm(range(7500)):
+    tmp = test[test.fragment_id == i][:60]
+    t[i, :, :, 0] = resample(tmp[use_feat], 60, np.array(tmp.time_point))[0]
 
 
 class LeNet(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv1d(in_channels=1, out_channels=6, kernel_size=3, padding=0)
-        self.conv2 = nn.Conv1d(in_channels=6, out_channels=16, kernel_size=3, padding=0)
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=6, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(in_channels=6, out_channels=16, kernel_size=3, padding=1)
         self.pool = nn.AvgPool1d(kernel_size=2, stride=2)
         self.fc1 = nn.Linear(in_features=336, out_features=256)
         self.fc2 = nn.Linear(in_features=256, out_features=19)
 
     def forward(self, x):
-        x = self.conv1(x)
+        x = nn.Conv1d(in_channels=60, out_channels=64, kernel_size=3, padding=1)(x)
         x = F.relu(x)
-        x = self.pool(x)
-        x = self.conv2(x)
+
+        x = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=1)(x)
         x = F.relu(x)
-        x = x.view(-1, 336)
-        x = self.fc1(x)
+
+        x = nn.MaxPool2d()(x)
+        x = nn.Dropout(0.2)(x)
+        x = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, padding=1)(x)
         x = F.relu(x)
-        x = self.fc2(x)
+        x = nn.Dropout(0.3)(x)
+
+        x = nn.Conv1d(in_channels=256, out_channels=512, kernel_size=3, padding=1)(x)
+        x = F.relu(x)
+        x = nn.MaxPool2d()(x)
+        x = nn.Dropout(0.5)(x)
         x = F.softmax(input=x, dim=1)
+
+        # x = self.pool(x)
+        # x = self.conv2(x)
+        # x = F.relu(x)
+        # x = x.view(-1, 336)
+        # x = self.fc1(x)
+        # x = F.relu(x)
+        # x = self.fc2(x)
+        # x = F.softmax(input=x, dim=1)
         return x
 
     def num_f(self, x):
@@ -88,49 +95,111 @@ class LeNet(nn.Module):
         return ret
 
 
-model = LeNet()
+model = nn.Sequential(nn.Conv2d(in_channels=60, out_channels=64, kernel_size=3, padding=1),
+                      nn.ReLU(),
+                      nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+                      nn.ReLU(),
 
+                      nn.MaxPool2d((2, 2)),
+                      nn.Dropout(0.2),
+
+                      nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+                      nn.ReLU(),
+
+                      nn.Dropout(0.3),
+
+                      nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+                      nn.ReLU(),
+                      nn.MaxPool2d((2, 2)),
+                      nn.Dropout(0.5),
+                      nn.Softmax()
+                      )
+
+kfold = StratifiedKFold(5, random_state=2020, shuffle=True)
+
+proba_t = np.zeros((7500, 19))
+for fold, (xx, yy) in enumerate(kfold.split(x, y)):
+    print(xx.shape, yy.shape)
+    x_trn = x[xx]
+
+#     # y_ = to_categorical(y, num_classes=19)
+#     model = LeNet()
+#     model.compile(loss='categorical_crossentropy',
+#                   optimizer=Adam(),
+#                   metrics=['acc'])
+#     plateau = ReduceLROnPlateau(monitor="val_acc",
+#                                 verbose=1,
+#                                 mode='max',
+#                                 factor=0.5,
+#                                 patience=8)
+#     early_stopping = EarlyStopping(monitor='val_acc',
+#                                    verbose=0,
+#                                    mode='max',
+#                                    patience=18)
+#     checkpoint = ModelCheckpoint(f'fold{fold}.h5',
+#                                  monitor='val_acc',
+#                                  verbose=0,
+#                                  mode='max',
+#                                  save_best_only=True)
+#     model.fit(x[xx], y_[xx],
+#               epochs=500,
+#               batch_size=256,
+#               verbose=1,
+#               shuffle=True,
+#               validation_data=(x[yy], y_[yy]),
+#               callbacks=[plateau, early_stopping, checkpoint])
+#     model.load_weights(f'fold{fold}.h5')
+#     proba_t += model.predict(t, verbose=0, batch_size=1024) / 5.
+# sub.behavior_id = np.argmax(proba_t, axis=1)
+# from datetime import datetime
+#
+# sub.to_csv(f'{datetime.now().strftime("%Y%m%d_%H%M%S")}_submit_cnn4.1_C.csv', index=False)
+
+
+model = LeNet()
+#
 certion = nn.CrossEntropyLoss()
 epochs = 5000
 kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=2020)
 for fold, (trn_idx, val_idx) in enumerate(kfold.split(train_x, train_y)):
     x_trn, y_trn, x_val, y_val = train_x.iloc[trn_idx], train_y.iloc[trn_idx], train_x.iloc[val_idx], train_y.iloc[
-        val_idx]
-    # x_trn = torch.Tensor(np.array(x_trn)).unsqueeze(1)
-    # x_val = torch.Tensor(np.array(x_val)).unsqueeze(1)
-    # y_trn = torch.Tensor(np.array(y_trn, dtype='int')).long()
-    # y_val = torch.Tensor(np.array(y_val, dtype='int')).long()
-
-    x_trn = torch.from_numpy(np.array(x_trn)).unsqueeze(1)
-    x_val = torch.from_numpy(np.array(x_val)).unsqueeze(1)
-    y_trn = torch.from_numpy(np.array(y_trn, dtype='int')).long()
-    y_val = torch.from_numpy(np.array(y_val, dtype='int')).long()
-
-    # print(x_trn.shape)
-    max_acc = 0
-    early_stop = 0
-    SGD = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    for epoch in range(1, epochs + 1):
-        model.train()
-        SGD.zero_grad()
-        output = model(x_trn)
-        loss = certion(output, y_trn)
-        loss.backward()
-        SGD.step()
-
-        model.eval()
-        output_ = model(x_val)
-        output_ = torch.argmax(output_, dim=1)
-        cnt = sum(output_ == y_val).item()
-        len_dataset = len(y_val)
-        acc = cnt / len_dataset * 100
-        print("Epoch{}: Accu in val_set is {}.".format(epoch, acc))
-
-        if max_acc < acc:
-            max_acc = acc
-            early_stop = 0
-        else:
-            early_stop += 1
-            if early_stop >= 50:
-                break
-
+#         val_idx]
+#     # x_trn = torch.Tensor(np.array(x_trn)).unsqueeze(1)
+#     # x_val = torch.Tensor(np.array(x_val)).unsqueeze(1)
+#     # y_trn = torch.Tensor(np.array(y_trn, dtype='int')).long()
+#     # y_val = torch.Tensor(np.array(y_val, dtype='int')).long()
+#
+#     x_trn = torch.from_numpy(np.array(x_trn)).unsqueeze(1)
+#     x_val = torch.from_numpy(np.array(x_val)).unsqueeze(1)
+#     y_trn = torch.from_numpy(np.array(y_trn, dtype='int')).long()
+#     y_val = torch.from_numpy(np.array(y_val, dtype='int')).long()
+#
+#     # print(x_trn.shape)
+#     max_acc = 0
+#     early_stop = 0
+#     SGD = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+#     for epoch in range(1, epochs + 1):
+#         model.train()
+#         SGD.zero_grad()
+#         output = model(x_trn)
+#         loss = certion(output, y_trn)
+#         loss.backward()
+#         SGD.step()
+#
+#         model.eval()
+#         output_ = model(x_val)
+#         output_ = torch.argmax(output_, dim=1)
+#         cnt = sum(output_ == y_val).item()
+#         len_dataset = len(y_val)
+#         acc = cnt / len_dataset * 100
+#         print("Epoch{}: Accu in val_set is {}.".format(epoch, acc))
+#
+#         if max_acc < acc:
+#             max_acc = acc
+#             early_stop = 0
+#         else:
+#             early_stop += 1
+#             if early_stop >= 50:
+#                 break
+#
+certion = nn.CrossEntropyLoss()
--- data/tmp/learn_pytorch.py
+++ data/tmp/learn_pytorch.py
@@ -1,8 +1,10 @@
-import torchtext
-from torchtext.vocab import Vectors
+# import torchtext
+# from torchtext.vocab import Vectors
 import torch
 import numpy as np
 import random
+import torch.nn.functional as F
+import torch.nn as nn
 
 USE_CUDA = torch.cuda.is_available()
 
@@ -18,24 +20,49 @@ EMBEDDING_SIZE = 650
 MAX_VOCAB_SIZE = 50000
 
 in_channels = 3
+
+
 class CNN_NET(torch.nn.Module):
     def __init__(self):
-            super(CNN_NET,self).__init__()
-            self.conv1 = nn.Sequential(
-                    nn.Conv2d(
-                        in_channels=in_channels,              # input height  数字识别是灰白照片，只有一个channel
-                        out_channels=16,            # n_filters
-                        kernel_size=3,              # filter size
-                        stride=1,                   # filter movement/step
-                        padding=1,                  # if want same width and length of this image after con2d, padding=(kernel_size-1)/2 if stride=1
-                    ),
-                nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
-                # nn.ReLU(),
-                # nn.AvgPool2d(kernel_size=2),
-            )
-import torch.nn as nn
+        super(CNN_NET, self).__init__()
+        # self.conv1 = nn.Sequential(
+        #     nn.Conv2d(
+        #         in_channels=60,  # input height  数字识别是灰白照片，只有一个channel
+        #         out_channels=128,  # n_filters
+        #         kernel_size=3,  # filter size
+        #         stride=1,  # filter movement/step
+        #         padding=1,
+        #         # if want same width and length of this image after con2d, padding=(kernel_size-1)/2 if stride=1
+        #     ),
+        #     nn.Conv2d(in_channels=128, out_channels=32, kernel_size=3, padding=1),
+        #     # nn.ReLU(),
+        #     # nn.AvgPool2d(kernel_size=2),
+        # )
+        # x = F.softmax(input=x, dim=1)
+
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=60, out_channels=64, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+                                   nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+
+                                   nn.MaxPool2d((2, 2)),
+                                   nn.Dropout(0.2),
+
+                                   nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+
+                                   nn.Dropout(0.3),
+
+                                   nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+                                   nn.MaxPool2d((2, 2)),
+                                   nn.Dropout(0.5),
+                                   nn.Softmax()
+                                   )
+
+
 net_test = CNN_NET()
 #
-test_data = torch.randn(3,in_channels,9,9)
+test_data = torch.randn(200, 60, 12, 1)
 out1 = net_test.conv1(test_data)
 print(out1.shape)
